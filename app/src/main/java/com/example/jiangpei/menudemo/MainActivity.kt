7ee496b16282cba3ed4975f3f8fc16da93aec599
Jiang Pei <18917944949@189.cn>
2018-03-19 16:30:38 +0800

    菜单演示

--- app/src/main/java/com/example/jiangpei/menudemo/MainActivity.kt
+++ app/src/main/java/com/example/jiangpei/menudemo/MainActivity.kt
@@ -6,19 +6,25 @@ import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registerForContextMenu(label1)
     }
 
     override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
         super.onCreateContextMenu(menu, v, menuInfo)
 
-
+        if (v?.id == R.id.label1) {
+            menuInflater.inflate(R.menu.label_menu, menu)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -34,6 +40,16 @@ class MainActivity : AppCompatActivity() {
             R.id.item2 -> Toast.makeText(this, menuItem.title, Toast.LENGTH_SHORT).show()
             R.id.item3 -> Toast.makeText(this, menuItem.title, Toast.LENGTH_SHORT).show()
             R.id.item4 -> Toast.makeText(this, menuItem.title, Toast.LENGTH_SHORT).show()
+
+            R.id.labelItem1 -> Toast.makeText(this, menuItem.title, Toast.LENGTH_SHORT).show()
+            R.id.labelItem2 -> Toast.makeText(this, menuItem.title, Toast.LENGTH_SHORT).show()
         }
+    }
+
+
+    fun popup_menu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.menuInflater.inflate(R.menu.label_menu, popupMenu.menu)
+        popupMenu.show()
     }
 }
